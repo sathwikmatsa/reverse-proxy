dd590b381a46598e102de5d9e76c36247cd6f1b2
Sathwik Matsa <sathwikmatsa@gmail.com>
2020-07-05 00:55:35 +0530

    setup forward proxy

--- app.js
+++ app.js
@@ -1,63 +1,16 @@
-/*
-  reverse-proxy.js: Example of reverse proxying (with HTTPS support)
-  Copyright (c) 2015 Alberto Pose <albertopose@gmail.com>
-
-  Permission is hereby granted, free of charge, to any person obtaining
-  a copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 const PORT = process.env.PORT || 8213;
 console.log("PORT ENV: ", PORT);
 
-var http = require("http"),
-  net = require("net"),
-  httpProxy = require("http-proxy"),
-  url = require("url");
+const http = require("http");
+const httpProxy = require("http-proxy");
+console.log("libs ok");
 
-console.log("libs initialized.");
+const proxy = httpProxy.createProxyServer({});
+console.log("proxy ok");
 
-var proxy = httpProxy.createServer();
-console.log("proxy initialized");
-
-var server = http
+http
   .createServer(function (req, res) {
-    var parsedUrl = url.parse(req.url);
-    var target = parsedUrl.protocol + "//" + parsedUrl.hostname;
-    proxy.web(req, res, { target: target, secure: false });
+    proxy.web(req, res, { target: req.url });
   })
   .listen(PORT);
-
-console.log("server initialized");
-
-server.on("connect", function (req, socket) {
-  var serverUrl = url.parse("https://" + req.url);
-
-  var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function () {
-    socket.write(
-      "HTTP/1.1 200 Connection Established\r\n" +
-        "Proxy-agent: Node-Proxy\r\n" +
-        "\r\n"
-    );
-    srvSocket.pipe(socket);
-    socket.pipe(srvSocket);
-  });
-});
-
-// Test with:
-// curl -vv -x http://127.0.0.1:8213 https://www.google.com
-// curl -vv -x http://127.0.0.1:8213 http://www.google.com
+console.log("server ok");
